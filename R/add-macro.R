2cfa96bf894ed93bf6755c7fefa81f975108f31c
Dennis Alexis Valin Dittrich <dd+github@dr-dittrich.de>
2023-01-10 18:18:05 +0100

    update built

---
+++ R/add-macro.R
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 davd
+#
+# conflr is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+#
+# conflr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See <http://www.gnu.org/licenses/> for more details.
+
+#' @export
+confl_add_node <- function(title = "", body = "") {
+
+ glue::glue('<div class="conflr-note" style="clear:both;width=32em;float:right; margin-left:2em">',
+   glue:glue('{{note:title={title}|icon=false}}{body}{{note}}') |> 
+   confl_contentbody_convert() |>
+   stringi::stri_replace_all_regex('<.?p>',''),
+   '</div>') -> x
+  
+ x
+}
+
+#' @export
+confl_add_tip <- function(title = "", body = "") {
+
+ glue::glue('<div class="conflr-note" style="clear:both;width=32em;float:right; margin-left:2em">',
+   glue:glue('{{tip:title={title}|icon=false}}{body}{{tip}}') |> 
+   confl_contentbody_convert() |>
+   stringi::stri_replace_all_regex('<.?p>',''),
+   '</div>') -> x
+  
+ x
+}
